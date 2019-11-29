65af2c6d82b4840ec0759b8fb92f3e298c351ee9
Matt Purdy <matt9986@gmail.com>
2019-11-29 15:49:18 -0800

    Minor updates, fix images, add snake

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -1,14 +1,25 @@
 $(function(){
-  $("#openAsteroidModal").on("click", function(){
-    $("#asteroidModal").modal()
-  });
-  var game;
+  var asteroidGame;
   $("#asteroidModal").on("shown.bs.modal", function(){
-    if (game) {
-      game.restart();
+    if (asteroidGame) {
+      asteroidGame.restart();
     }else{
-      game = new Asteroids.Game();
-      game.start();
+      asteroidGame = new Asteroids.Game();
+      asteroidGame.start();
+    }
+  });
+  var faviconSnake;
+  $("#asteroidModal").on("shown.bs.modal", function(){
+    if (!faviconSnake) {
+      JsSnake.SnakeGame.snake_game(new JsSnake.FaviconDisplay.FaviconDisplay(document))
+      faviconSnake = true;
+    }
+  });
+  var canvasSnake;
+  $("#asteroidModal").on("shown.bs.modal", function(){
+    if (!canvasSnake) {
+      JsSnake.SnakeGame.snake_game(new JsSnake.CanvasDisplay.CanvasDisplay(document.getElementById('snake-game-target')))
+      asteroidGame = true;
     }
   });
 });
